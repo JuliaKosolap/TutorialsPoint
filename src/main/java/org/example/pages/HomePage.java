0c828ced22dc47de7be1260e2bab79a497515769
user <jkosolap@gmail.com>
2025-03-10 20:45:34 +0200

    Refactoring

--- src/main/java/org/example/pages/HomePage.java
+++ src/main/java/org/example/pages/HomePage.java
@@ -1,8 +1,6 @@
 package org.example.pages;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 
